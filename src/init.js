b9bae76c1e662d2c21cd488fe1efec119ff2c9da
Naddiya <nadiamedkouri@gmail.com>
2021-05-19 00:16:28 +0200

    fix: hide modal

--- src/init.js
+++ src/init.js
@@ -3,8 +3,6 @@ import game, { handleUserAction } from "./gameState";
 import { TICK_RATE } from './constants';
 
 async function init() {
-  console.log('starting game');
-
   initButtons(handleUserAction);
 
   let nexTimeToTick = Date.now();
